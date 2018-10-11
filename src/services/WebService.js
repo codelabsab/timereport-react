b62cc18983fef1d1ae84c7cf170c56ad31fe0567
sakib3 <sakib3@gmail.com>
2018-10-11 15:49:18 +0200

    error handler & nav bar

--- src/services/WebService.js
+++ src/services/WebService.js
@@ -6,9 +6,10 @@ export function getAccessToken(url) {
 
 export function getUsers() {
     let urlSegemntAccessToken = '?access_token=' + StorageService.getAccessToken();
+    console.log(urlSegemntAccessToken);
     return fetch(API_ROOT + '/api/users' + urlSegemntAccessToken)
         .then(res => res.json())
-        .then(handleErrors);
+        .then(handleErrors2);
 }
 
 export function getTimeReport(query) {
@@ -19,10 +20,18 @@ export function getTimeReport(query) {
         headers: { 'Content-Type': 'application/json' }
     })
         .then(res => res.json())
-        .then(handleErrors);
+        .then(handleErrors2);
 }
 function handleErrors(response) {
+    console.log('slack api', response);
     if (!response.ok) {
+        throw Error(response.error);
+    }
+    return response;
+}
+function handleErrors2(response) {
+    console.log('backend api',response);
+    if (response.error) {
         throw Error(response.error);
     }
     return response;
