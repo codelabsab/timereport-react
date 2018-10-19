c34cd21b8398e398cc1c72bee16db33634bd3099
sakib3 <sakib3@gmail.com>
2018-10-19 10:21:50 +0200

    test app component

--- src/components/TestApp.js
+++ src/components/TestApp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, HashRouter, Route, Link } from "react-router-dom";
-import Home from '../containers/Home';
+
 
 
 import {
@@ -12,24 +12,28 @@ import {
 } from '../actions/index'
 import { createStore } from 'redux'
 import timeReportApp from '../reducers'
+
+
+import { Provider } from 'react-redux'
+
+// console.log(store.getState())
+// const unsubscribe = store.subscribe(() => console.log('unsubscribe ->',store.getState()))
+// // Dispatch some actions
+// store.dispatch(setAuthToken('Auth Token'))
+// store.dispatch(setUsers([{id:1}, {id:2}]))
+// store.dispatch(setTimeReport([{timereport:1}]))
+// store.dispatch(setUser("user"))
+// store.dispatch(setDateTimePeriod('start_date', 'end_date'))
+// // Stop listening to state updates
+// unsubscribe()
 const store = createStore(timeReportApp);
-
-console.log(store.getState())
-const unsubscribe = store.subscribe(() => console.log('unsubscribe ->',store.getState()))
-// Dispatch some actions
-store.dispatch(setAuthToken('Auth Token'))
-store.dispatch(setUsers([{id:1}, {id:2}]))
-store.dispatch(setTimeReport([{timereport:1}]))
-store.dispatch(setUser("user"))
-store.dispatch(setDateTimePeriod('start_date', 'end_date'))
-// Stop listening to state updates
-unsubscribe()
-
  const App = () => (
-  <HashRouter>
-    <div>
-      <Route exact path="/" component={Home} />
-    </div>
-  </HashRouter>
+
+    
+     <Provider store={store}>
+        <div>App</div>
+    </Provider>
+ 
+
  );
  export default App;
