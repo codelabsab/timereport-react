bd530a8b511577ca7e67bd082b2ad7357cde652c
sakib3 <sakib3@gmail.com>
2018-11-07 10:18:26 +0100

    added buttons in home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
-const urlEncodSlackRedirectUri = SLACK_REDIRECT_URI.replace("#","%23");
-const url = SLACK_ROOT_API_URL+"/oauth/authorize?scope=read&client_id=" + SLACK_CLIENT_ID + "&redirect_uri="+urlEncodSlackRedirectUri+"&team=TD61YHFEF";
+const urlEncodSlackRedirectUri = SLACK_REDIRECT_URI.replace("#", "%23");
+const url = SLACK_ROOT_API_URL + "/oauth/authorize?scope=read&client_id=" + SLACK_CLIENT_ID + "&redirect_uri=" + urlEncodSlackRedirectUri + "&team=TD61YHFEF";
 
 const Home = () => (
-    <div className="card text-center">
-      <div className="card-body">
-        <h1 className="card-title">Sign in</h1>
-        <p className="card-text">You will be redirected after providing your credentials.</p>
+  <div>
+    {/* <div className="text-center">
         <a className="btn btn-outline-light" href={url}>
           <img className="img-fluid" src={require('./../images/slack2.png')} />
         </a>
-      </div>
+ 
+    </div> */}
+    <div className="text-center">
+      <a href={url} className="btn btn-outline-secondary">SignIn With Slack</a>&nbsp;
+      <a href="#/signin" className="btn btn-outline-secondary">Regular SignIn</a>&nbsp;
+      <a href="#/signup" className="btn  btn-outline-secondary">SignUp</a>
     </div>
-  );
-  
-  export default Home;
+  </div>
+);
+
+export default Home;
