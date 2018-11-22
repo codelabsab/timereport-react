576bb303cd584f07d684e6521e21d8681a5f305b
sakib3 <sakib3@gmail.com>
2018-11-22 08:09:59 +0100

    store non slack user cred into storage session

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -21,8 +21,21 @@ export default class SignIn extends Component {
             let isSignIn = !!email;
             SlackService.userLookupByEmail(email)
                 .then(slack_user => {
-                    if(slack_user == null){
-                        this.handleError(new Error('Email is not found in slack!'));
+                    if (slack_user == null) {
+                        let payload = CognitoUserService.getUser().signInUserSession.idToken.payload;
+                        let email = payload['email'];
+                        let domain = email.replace(/.*@/, "");
+                        let non_slack_user = {
+                            email: email,
+                            id: null,
+                            name: payload['cognito:username'],
+                            team_id: domain
+                        };
+
+                        StorageService.setSlackUser(non_slack_user);
+                        StorageService.setAccessToken(non_slack_user.team_id);
+
+                        //this.handleError(new Error('Email is not found in slack!'));
                         return;
                     }
                     StorageService.setSlackUser(slack_user);
@@ -31,7 +44,7 @@ export default class SignIn extends Component {
                 });
         });
     }
-    
+
     handleError = (e) => {
         let errorMessage = 'Error : ' + (e.message || 'Error Occured');
         NotifyService.notify(errorMessage);
@@ -61,11 +74,11 @@ export default class SignIn extends Component {
                     </div>
                     <div className="form-group row">
                         <div className="col-12 col-sm-8 offset-sm-2">
-                            <a className="btn-outline" href="/" style={{marginRight:'.5rem', color:'#343a40'}}>
+                            <a className="btn-outline" href="/" style={{ marginRight: '.5rem', color: '#343a40' }}>
                                 <span className="oi oi-chevron-left"></span>
                             </a>
                             <button type="submit" className="btn btn-dark" onClick={(e) => this.doSignIn(e)}>Submit</button>
-                            <a className="btn btn-link" href="#/forgotpassword" style={{color:'#343a40'}}>Forgot Password</a>
+                            <a className="btn btn-link" href="#/forgotpassword" style={{ color: '#343a40' }}>Forgot Password</a>
                         </div>
                     </div>
                 </form>
@@ -73,10 +86,10 @@ export default class SignIn extends Component {
                 {/* <br /> <br />
                 <div className="form-group row">
                     <div className="col-12 col-sm-8 offset-sm-2">
-                        <button className="btn btn-dark" onClick={() => console.log(CognitoUserService.getUser())}> Check Cognito User</button>
+                        <button className="btn btn-dark" onClick={() => console.log(CognitoUserService.getUser().signInUserSession.idToken.payload)}> Check Cognito User</button>
                     </div>
-                </div> */}
-                <NotifyContainer />
+                </div>
+                <NotifyContainer /> */}
             </div>
         );
     }
