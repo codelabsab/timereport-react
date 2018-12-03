a69858c20b5befc6daf182595d7870ca48bbd134
sakib3 <sakib3@gmail.com>
2018-12-03 14:24:22 +0100

    remove reducers

--- src/components/TestApp.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import DashBoard from '../containers/DashBoard.red';
-
-export default class TestApp extends Component {
-  render(){
-    return <DashBoard/>;
-  }
-}
--- src/containers/DatePicker.red.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux'
-import DatePicker from '../components/DatePicker.red'
-import { pickDate } from '../actions'
-
-
-export default connect(null, { pickDate })(DatePicker)
